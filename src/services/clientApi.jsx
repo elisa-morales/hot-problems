e32854b432b3b5e202b0b26af6881a4545ff8a85
elisa-morales <morales.elisa@yahoo.it>
2023-07-16 11:37:21 +0200

    Update clientApi.jsx

--- src/services/clientApi.jsx
+++ src/services/clientApi.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL
+const url = import.meta.env.VITE_API_URL
+axios.defaults.baseURL = url
 
 const clientApi = async (id) => {
   try {
