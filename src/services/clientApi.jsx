796df814903ac62a67217ca0385eb9620c12fd0d
elisa-morales <morales.elisa@yahoo.it>
2023-07-16 11:18:28 +0200

    Update clientApi.jsx

--- src/services/clientApi.jsx
+++ src/services/clientApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = "https://global-warming.org/api/"
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 const clientApi = async (id) => {
   try {
